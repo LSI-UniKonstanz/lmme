0f86143affb1b61025d44a894d351a606f4572ba
Michael Aichem <61273192+michaelaichem@users.noreply.github.com>
2020-06-18 20:36:32 +0200

    - compartment subsystem now uses full name of compartment if available

--- src/org/vanted/addons/lmme/decomposition/CompartmentMMDecomposition.java
+++ src/org/vanted/addons/lmme/decomposition/CompartmentMMDecomposition.java
@@ -1,29 +1,25 @@
 /*******************************************************************************
  * LMME is a VANTED Add-on for the exploration of large metabolic models.
  * Copyright (C) 2020 Chair for Life Science Informatics, University of Konstanz
- * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  ******************************************************************************/
 package org.vanted.addons.lmme.decomposition;
 
-import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import org.AttributeHelper;
 import org.FolderPanel;
@@ -33,7 +29,6 @@ import org.graffiti.graph.Node;
 import org.vanted.addons.lmme.core.LMMEController;
 import org.vanted.addons.lmme.graphs.BaseGraph;
 import org.vanted.addons.lmme.graphs.SubsystemGraph;
-import org.vanted.addons.lmme.ui.LMMETab;
 
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.ios.sbml.SBML_Constants;
 
@@ -44,29 +39,38 @@ import de.ipk_gatersleben.ag_nw.graffiti.plugins.ios.sbml.SBML_Constants;
  * @author Michael Aichem
  */
 public class CompartmentMMDecomposition extends MMDecompositionAlgorithm {
-
+	
 	private FolderPanel fp;
-
+	
 	@Override
 	protected ArrayList<SubsystemGraph> runSpecific(HashSet<Node> alreadyClassifiedNodes) {
-
+		
 		HashSet<String> compartments = new HashSet<String>();
-
+		HashMap<String, String> compartmentAbbreviations = new HashMap<String, String>();
+		
 		BaseGraph baseGraph = LMMEController.getInstance().getCurrentSession().getBaseGraph();
-
+		
 		for (Node speciesNode : baseGraph.getSpeciesNodes()) {
 			if (!alreadyClassifiedNodes.contains(speciesNode)) {
 				if (AttributeHelper.hasAttribute(speciesNode, SBML_Constants.SBML, SBML_Constants.COMPARTMENT)) {
-					compartments.add((String) AttributeHelper.getAttributeValue(speciesNode, SBML_Constants.SBML,
-							SBML_Constants.COMPARTMENT, "", ""));
+					String compNameShort = (String) AttributeHelper.getAttributeValue(speciesNode, SBML_Constants.SBML,
+							SBML_Constants.COMPARTMENT, "", "");
+					compartments.add(compNameShort);
+					if (AttributeHelper.hasAttribute(speciesNode, SBML_Constants.SBML, SBML_Constants.SPECIES_COMPARTMENT_NAME)) {
+						String compNameLong = (String) AttributeHelper.getAttributeValue(speciesNode, SBML_Constants.SBML,
+								SBML_Constants.SPECIES_COMPARTMENT_NAME, "", "");
+						compartmentAbbreviations.put(compNameShort, compNameLong);
+					}
 				}
 			}
 		}
-
+		
 		ArrayList<SubsystemGraph> subsystems = new ArrayList<SubsystemGraph>();
-
+		
 		for (String compartment : compartments) {
-			SubsystemGraph subsystem = new SubsystemGraph("Compartment " + compartment, new HashSet<>(),
+			String compLongName = compartmentAbbreviations.get(compartment);
+			String compName = compLongName == null ? "Compartment " + compartment : compLongName;
+			SubsystemGraph subsystem = new SubsystemGraph(compName, new HashSet<>(),
 					new HashSet<>(), new HashSet<>());
 			for (Node speciesNode : baseGraph.getSpeciesNodes()) {
 				if (compartment.equals((String) AttributeHelper.getAttributeValue(speciesNode, SBML_Constants.SBML,
@@ -93,15 +97,15 @@ public class CompartmentMMDecomposition extends MMDecompositionAlgorithm {
 		}
 		return subsystems;
 	}
-
+	
 	@Override
 	public boolean requiresCloning() {
 		return true;
 	}
-
+	
 	@Override
 	public FolderPanel getFolderPanel() {
-
+		
 		if (this.fp != null) {
 			updateFolderPanel();
 		} else {
@@ -110,21 +114,21 @@ public class CompartmentMMDecomposition extends MMDecompositionAlgorithm {
 			fp.addGuiComponentRow(row, true);
 		}
 		return fp;
-
+		
 	}
-
+	
 	@Override
 	public void updateFolderPanel() {
 		// Do nothing.
 	}
-
+	
 	@Override
 	public String getName() {
 		return "Compartment Decomposition";
 	}
-
+	
 	public boolean requiresTransporterSubsystem() {
 		return true;
 	}
-
+	
 }
